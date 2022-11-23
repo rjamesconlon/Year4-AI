457c1d4701542823b1a16811ff86c8ea60a25fcf
Ronnnie Conlon <56613512+s00200671@users.noreply.github.com>
2022-11-23 15:28:37 +0000

    better at checking counters and wins

--- assignment2/xogame.pl
+++ assignment2/xogame.pl
@@ -3,21 +3,13 @@
 %
 % X's and O's game using A* search
 
-% representation of the board as a list of facts
-%
-% representation of the board as a list of lists
-%board([[1, 1, n, 1], [1, 2, n, 2], [1, 3, n, 1],
-%       [2, 1, n, 2], [2, 2, n, 4], [2, 3, n, 2],
-%       [3, 1, n, 1], [3, 2, n, 2], [3, 3, n, 1]]).
-
-% board(<row>, <column>, <x/o or n>, <heuristic value>).
-
 % board
 % --------------------------------------------------
 cell(1, 1, x, 1). cell(1, 2, n, 2). cell(1, 3, x, 1).
 cell(2, 1, n, 2). cell(2, 2, n, 4). cell(2, 3, n, 2).
-cell(3, 1, n, 1). cell(3, 2, n, 2). cell(3, 3, o, 1).
+cell(3, 1, o, 1). cell(3, 2, n, 2). cell(3, 3, o, 1).
 % --------------------------------------------------
+
 % terminal states for the board
 
 % 3 in a row for row
@@ -45,32 +37,63 @@ win(P) :-
 
 % full board
 
-fullboard(P) :-
+boardNotFull(P) :-
     cell(_, _, P, _),
-    ((P == x);(P == o)).
+    P = n.
 
 % make list of possible moves depending on input
 getCells(L, N) :- findall(cell(X, Y, N, H), cell(X, Y, N, H), L).
 
-% check surrounding cells
-counterCells(cell(X, Y, _, _), Player, L) :-
+% Get almost wins (2 out of 3 for a win condition)
+% this checks rows and columns based off of given cell by finding all of a certain players inputs
+% using surrounding cells
+% then checks what the length of the list is
+% is the list is 2 then clearly the player/opponent has 2 out of 3, meaning the empty cell
+% is a counter cell
+counterCell(cell(X, Y, N, H), Player) :-
     (findall(cell(X, B, Player, Heuristic), cell(X, B, Player, Heuristic), L),
     length(L, 2))
     ;
     (findall(cell(A, Y, Player, Heuristic), cell(A, Y, Player, Heuristic), L),
-    length(L, 2)
-    )
+    length(L, 2)),
+    assert(cell(X, Y, N, 5)),
+    retract(cell(X, Y, N, H)).
+
+winCell(cell(X, Y, N, H), Player) :-
+    (findall(cell(X, B, Player, Heuristic), cell(X, B, Player, Heuristic), L),
+    length(L, 2))
     ;
-    (find
-
-
-
-
-bestPossibleMove(C, Max) :-
-    getCells(L, n).
+    (findall(cell(A, Y, Player, Heuristic), cell(A, Y, Player, Heuristic), L),
+    length(L, 2)),
+    assert(cell(X, Y, N, 10)),
+    retract(cell(X, Y, N, H)).
 
 % look for optimal move
-optimal_move() :-
-    todo().
-
-
+optimal_move(WinningCells, CounterCells, OptimalMove) :-
+    %check if board is valid, anyone won yet?
+    %\+ win(_),
+    %check if board is full if no one has won
+    %\+ fullboard(_),
+    %check free spaces
+    getCells(L, n),
+    writeln(L),
+    %using free spaces, check win spaces
+    findall(Cell, (member(Cell, L), counterCell(Cell, x)), WinningCells),
+    getCells(M, n),
+    writeln(L),
+    %if no win spaces, check counter spaces
+    findall(Cell, (member(Cell, M), counterCell(Cell, o)), CounterCells),
+    getCells(N, n),
+    writeln(L),
+    %if no counter spaces, choose highest heuristic val
+    
+    write("Winning Move(s): "),
+    writeln(WinningCells),
+    
+    write("Counter Moves(s): "),
+    writeln(CounterCells),
+    
+    write("Moves in order of heuristic val: "),
+    
+    OptimalMove = [WinningCells, CounterCells, L].
+    
